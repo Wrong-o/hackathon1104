8826069c9b8cd466f2333ec41f8a8b63c7f943c1
Wrong-o <ott_t@hotmail.com>
2024-11-04 13:25:41 +0100

    api access working

---
+++ question.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import openai
+import os
+
+
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+openai.api_key = os.getenv("API_KEY")
+
+
+
+question = input("Fråga mig: ")
+response = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo",
+    messages = [
+        {"role": "system", "content": "You think you are always right, harsh and desicive. Low regards for weakness"},
+        {"role": "user", "content": question},
+    ],
+
+)
+print(response["choices"][0]["message"]["content"])
+
+
